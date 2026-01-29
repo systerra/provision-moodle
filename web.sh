3395c9487c8a2b6a7ba6e6b04ca69c42b3ce2852
systerra <noval.rizky002@gmail.com>
2026-01-29 17:46:39 +0700

    Update web.sh

--- web.sh
+++ web.sh
@@ -10,7 +10,7 @@ Building dependency tree... Done
 Reading state information... Done
 The following additional packages will be installed:
 EOF
-apt install -y -qq nginx >/dev/null 2>&1
+apt-get install -y nginx >/dev/null 2>&1
 
 echo ""
 cat <<'EOF'
@@ -20,7 +20,7 @@ Building dependency tree... Done
 Reading state information... Done
 The following additional packages will be installed:
 EOF
-apt-get install -y -qq \
+apt-get install -y \
 php8.2-fpm php8.2-cli php8.2-curl php8.2-zip php8.2-gd \
 php8.2-xml php8.2-intl php8.2-mbstring php8.2-xmlrpc php8.2-soap \
 php8.2-bcmath php8.2-exif php8.2-ldap php8.2-mysql unzip \
