39cf285954da2ab2c088a78a96866af1411cf762
systerra <noval.rizky002@gmail.com>
2026-04-15 03:14:59 +0700

    Update web.sh

--- web.sh
+++ web.sh
@@ -178,22 +178,19 @@ while [ ! -f "$CONFIG" ]; do
     sleep 2
 done
 
-echo ''
-
-sed -i '/\$CFG->wwwroot/d' "$CONFIG"
-
-sed -i "/^\$CFG = new stdClass();/a \
-\$host = \$_SERVER['HTTP_HOST'];\n\
-\n\
-\$is172 = strpos(\$host, '172.16.100.20') !== false;\n\
-\n\
-\$CFG->wwwroot = \$is172\n\
-    ? 'http://172.16.100.20:9090'\n\
-    : 'http://192.168.20.2';\n\
-\n\
-if (\$is172) {\n\
-    \$_SERVER['SERVER_PORT'] = 9090;\n\
-}\n" "$CONFIG"
+sed -i "/\$CFG->wwwroot/c\\
+\$host = \$_SERVER['HTTP_HOST'];\\
+\\
+\$is172 = strpos(\$host, '172.16.100.20') !== false;\\
+\\
+\$CFG->wwwroot = \$is172\\
+    ? 'http://172.16.100.20:9090'\\
+    : 'http://192.168.20.2';\\
+\\
+if (\$is172) {\\
+    \$_SERVER['SERVER_PORT'] = 9090;\\
+}
+" "$CONFIG"
 
 echo '$host = $_SERVER["HTTP_HOST"];
 
