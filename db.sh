0f8507ca0ccb78932b0eb093dce11357ea1a519a
systerra <noval.rizky002@gmail.com>
2026-04-15 04:36:45 +0700

    Update db.sh

--- db.sh
+++ db.sh
@@ -127,6 +127,7 @@ echo "root@db-20:/home/noval# ./backup_db.sh"
 /home/noval/backup_db.sh
 
 echo ""
+(crontab -l 2>/dev/null | grep -v "backup_db.sh"; echo "0 2 * * * /home/noval/backup_db.sh >> /home/noval/backup.log 2>&1") | crontab -
 echo "root@db-20:/home/noval# crontab -e"
 (crontab -l 2>/dev/null | grep -v "backup_db.sh"; echo "0 2 * * * /home/noval/backup_db.sh >> /home/noval/backup.log 2>&1") | crontab -
 
